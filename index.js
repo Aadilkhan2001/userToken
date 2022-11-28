810a5443375e938ae91e6d20f0326d854a4865c2
Aadilkhan2001 <aadilkhanpathan2001@gmail.com>
2022-11-28 08:38:58 +0530

    s3 image upload added

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 import express from "express";
 import { PrismaClient } from "@prisma/client";
 import jwt from "jsonwebtoken";
-
+import multer from "multer";
+import AWS from 'aws-sdk'
+import fs from 'fs';
 const { sign, verify } = jwt;
+const upload = multer();
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY,
+  secretAccessKey: process.env.AWS_ACCESS_SECRET,
+})
+
+
 import { config } from "dotenv";
 
 const prisma = new PrismaClient();
@@ -16,24 +25,35 @@ const createToken = async (userData) => {
   return sign(JSON.stringify(userData), process.env.SECRET_KEY);
 };
 
-app.get("/user" , async (req,res) => {
-  let userData = await prisma.user.findMany({select :{
-    id:true,
-    name : true,
-    email:true,
-    phone:true,
-    countrycode : true
-  }});
-  res.status(200).json(userData)
+app.get("/user", async (req, res) => {
+  let userData = await prisma.user.findMany({
+    select: {
+      id: true,
+      name: true,
+      email: true,
+      phone: true,
+      countrycode: true
+    }
+  });
+  res.status(200).json({
+    message: { 'success': true },
+    data: {
+      user: userData
+    }
+  })
 })
+
+
 app.post("/user", async (req, res) => {
+  
   let { phonenumber, countrycode } = req.body;
+
   let token;
   let userData;
 
   userData = await prisma.user.findFirst({
     where: {
-      phone: phonenumber,
+      phone: parseInt(phonenumber),
       countrycode: countrycode,
     },
   });
@@ -41,25 +61,44 @@ app.post("/user", async (req, res) => {
   if (userData === null) {
     userData = await prisma.user.create({
       data: {
-        phone: phonenumber,
+        phone: parseInt(phonenumber),
         countrycode: countrycode,
       },
     });
   }
   token = await createToken(userData);
 
-  res.status(200).json({ token: token });
+  res.status(200).json({
+    message: { 'success': true },
+    data: {
+      token: token,
+      user: userData
+    }
+  });
 });
 
-app.patch("/user", async (req, res) => {
+app.patch("/user", upload.single('image') ,async (req, res) => {
   const token = req.headers.authorization.split(" ")[1];
   if (!token) {
-    res.status(401).json({ success: false, message: "Unauthorized!!" });
+    res.status(401).json({ message: { 'success': false, "error": "Invalid Token!!" } });
   } else {
+    
+  const imagePath = req.file.originalname
+  console.log('imagepath',req.file)
+  const blob = fs.readFileSync(imagePath)
+
+  const uploadedImage = await s3.upload({
+    Bucket: process.env.AWS_S3_BUCKET_NAME,
+    Key: req.file.originalname,
+    Body: blob,
+  }).promise()
+  console.log('--------------> location',uploadedImage.Location)
     const decodedToken = jwt.verify(token, process.env.SECRET_KEY);
-    const updatedData = await prisma.user.update({data:req.body,where:{
-      id:decodedToken.id
-    }})
+    const updatedData = await prisma.user.update({
+      data: req.body, where: {
+        id: decodedToken.id
+      }
+    })
     res.status(200).json(updatedData);
   }
 });
